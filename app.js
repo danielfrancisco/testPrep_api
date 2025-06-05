58f2c5172102d1649f6bf908c59b539d0ff5c60d
Daniel <danielfrancisco99@outlook.com>
2025-06-05 14:10:25 -0500

    loginPage

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const PORT = 3000;
 const tests_route = require('./routes/tests_route');
 const cors = require('cors');
 const create_user = require('./routes/createUser_route')
+const login_route = require('./routes/login_route')
 
 app.use(express.json());
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.send('Hello, World!');
 });
 
 app.use('/tests', tests_route);
 app.use('/create-user', create_user)
+app.use('/login', login_route);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ routes/login_route.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.post('/', (req, res) => {
+       const available = ['student', 'admin']
+       const{role} = req.body
+
+       // Safelist roles to avoid SQL injection
+       if (!available.includes(role)) {
+        return res.status(400).json({ error: 'Invalid role' });
+        }
+
+       const { name, email, password } = req.body.form;
+       const table = `${role}_table`
+       
+       const sql = `SELECT * FROM ${table} WHERE email = ? AND ${role}_password = ?;`
+       
+       db.query(sql, [email, password], (err, results) => {
+              if (err) {
+              return res.status(500).json({ error: 'Query failed', details: err });
+              }
+              res.json(results);
+              console.log(results)
+       });
+
+});
+
+module.exports = router;
+
